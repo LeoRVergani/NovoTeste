fe7c6c37adca6689ae2db3888b82cae001a6ef26
leorvergani <vergani@outlook.com.br>
2025-03-29 03:24:08 -0300

    :memo: update

--- obj/Debug/net9.0/NovoTeste.AssemblyInfo.cs
+++ obj/Debug/net9.0/NovoTeste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NovoTeste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b267f5f2bf3aebbb72b2a01aee3610119a631b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NovoTeste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NovoTeste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
