ae0964a406eaf2d6d8babc1605b57ba3c5f7672a
leorvergani <vergani@outlook.com.br>
2025-04-08 23:44:27 -0300

    :memo: update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+namespace NovoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            id = new Guid("63a11d56-00bd-41f2-a880-8bfebc98389f");
+            
+            Console.WriteLine(id.ToString().Substring(0, 8));
+
+            var texto = "Testando";
+            Console.WriteLine(texto.CompareTo("testando"));
+            Console.WriteLine(texto.Contains("Test"));
+
+            var texto2 = "Este texto é um teste";
+            Console.WriteLine(texto2.IndexOf("é"));
+            Console.WriteLine(texto2.LastIndexOf("s"));
+            Console.WriteLine(texto2.ToLower());
+            Console.WriteLine(texto2.ToUpper());
+            Console.WriteLine(texto2.Insert(5, "AQUI "));
+            Console.WriteLine(texto2.Remove(10, 5));
+            Console.WriteLine(texto2.Length);
+            Console.WriteLine(texto2.Replace("Este", "Isto"));
+            Console.WriteLine(texto2.Replace("e", "X"));
+
+            var divisao = texto2.Split(" ");
+            Console.WriteLine(divisao[0]);
+            Console.WriteLine(divisao[1]);
+            Console.WriteLine(divisao[2]);
+
+            var resultado = texto2.Substring(5, 5);
+            var resultado2 = texto2.Substring(5, texto.LastIndexOf("o"));
+            Console.WriteLine(resultado);
+            Console.WriteLine(resultado2);
+
+            Console.WriteLine(texto2.Trim());
+
+            texto2 += " aqui";
+
+            Console.WriteLine(texto2);
+
+            var texto3 = new StringBuilder();
+            texto3.Append("Este texto é um texto maluco");
+            texto3.Append(" texto maluco");
+            texto3.Append("Este  maluco");
+            texto3.Append("Este texto é  maluco");
+
+            texto3.ToString();
+            Console.WriteLine(texto3);
+
+
+
+
+        }
+    }
+}
--- obj/Debug/net9.0/NovoTeste.AssemblyInfo.cs
+++ obj/Debug/net9.0/NovoTeste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NovoTeste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b267f5f2bf3aebbb72b2a01aee3610119a631b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe7c6c37adca6689ae2db3888b82cae001a6ef26")]
 [assembly: System.Reflection.AssemblyProductAttribute("NovoTeste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NovoTeste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
